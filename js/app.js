be47893dfa1b984af622dee35e579ff5e608551e
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-05-06 20:40:51 +0300

    adding poems and images to them

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
 'use strict';
 
-var app = angular.module('App', ['ngRoute']).filter("nl2br", function($filter) {
-    return function(data) {
-        if (!data) return data;
-        return data.replace(/\n\r?/g, '<br />');
-    };
-});
+var app = angular.module('App', ['ngRoute', 'ngSanitize']);
